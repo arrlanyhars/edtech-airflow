b08028d941f580f7be55b4393962c44b2a9a57e7
lani268 <71999653+arrlanyhars@users.noreply.github.com>
2023-11-01 14:52:51 +0700

    Update dag_pipeline_airflow.py

--- dag_pipeline_airflow.py
+++ dag_pipeline_airflow.py
@@ -103,7 +103,6 @@ def extract_and_transform_data():
 
 
 	#LOAD
-
 	destination_table_id  = "sandbox-data-401207.technical_test_de.ArryandaMaulani_PapuaSelatanKabMerauke_technicalTestSkolla"
 	dataset_ref = client.dataset("technical_test_de", project="sandbox-data-401207")
 	table_ref = dataset_ref.table("ArryandaMaulani_PapuaSelatanKabMerauke_technicalTestSkolla")
